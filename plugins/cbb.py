5a64fd0c8e2b83ccfccf301d795dca797592e48a
Corico0 <126371308+Corico0@users.noreply.github.com>
2023-03-04 23:04:30 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/ropisaac'>This person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ GitHub repo : <a href='https://github.com/Corico0/Forex-Backtest-resources'>Click here</a>",
+            text = f"<strong>○ Creator : <a href='https://t.me/ropisaac'>This person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ GitHub repo : <a href='https://github.com/Corico0/Forex-Backtest-resources'>Click here</a> </strong>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
